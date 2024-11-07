be3da3a024eb7384f87ba32dbc7e8a2c5072344a
Guzman2304 <gquintero377@unab.edu.co>
2024-11-07 17:15:06 -0500

    corregi los colores del Login

--- app/src/main/java/com/edu/unab/diegocastro/ggaplication/Login.kt
+++ app/src/main/java/com/edu/unab/diegocastro/ggaplication/Login.kt
@@ -1,5 +1,6 @@
 package com.edu.unab.diegocastro.ggaplication
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,20 +27,24 @@ import com.edu.unab.diegocastro.ggaplication.ui.theme.GGAplicationTheme
 @Preview
 @Composable
 
-fun Login(){
+fun Login() {
     GGAplicationTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Column(
                 modifier = Modifier
                     .padding(innerPadding)
                     .padding(16.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center) {
+                    .fillMaxSize()
+                    .background(color = Color(0xFFA3D16A)),
+                verticalArrangement = Arrangement.Center
+            ) {
 
 
-                Card {
+                Card(modifier = Modifier.padding(10.dp)) {
                     Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = "LOGIN")
+                        Text(
+                            text = "LOGIN", Modifier.background(color = Color(0xFFA3D16A))
+                        )
 
                         OutlinedTextField(
                             value = "",
@@ -59,14 +66,16 @@ fun Login(){
 
                         Spacer(modifier = Modifier.height(400.dp))
 
-                        Button(onClick = { /*TODO*/ },
-                            modifier = Modifier.fillMaxWidth()) {
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(Color(0xFFA3D16A))
+                        ) {
                             Text(text = "Log In")
 
                         }
 
                         //OutlinedButton(onClick = ) {}
-                            
 
 
                     }
