910c67485b012a6b2c4085ac1a17770f0b806378
DiegoACx <dcastro405@unab.edu.co>
2024-11-11 16:11:46 -0500

    Colores Inscribirse Screen

--- app/src/main/java/com/edu/unab/diegocastro/ggaplication/InscribirseScreen.kt
+++ app/src/main/java/com/edu/unab/diegocastro/ggaplication/InscribirseScreen.kt
@@ -1,49 +1,45 @@
 package com.edu.unab.diegocastro.ggaplication
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun InscripcionScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFF8F8F8))
+            .background(Color(0xFFE1E5CE))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box (
+            Box(
                 modifier = Modifier
                     .size(48.dp)
                     .background(Color(0xFF66BB6A), shape = RoundedCornerShape(8.dp)),
@@ -57,45 +53,37 @@ fun InscripcionScreen() {
             }
             Spacer(modifier = Modifier.width(8.dp))
             Text(
-                text = "INSCRIBITE",
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.DarkGray
+                text = "INSCRIBETE",
+                fontSize = 24.sp,
+                color = Color(0xFFA3D16A),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
             )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-
-        Button (
-            onClick = { },
-            modifier = Modifier
-                .fillMaxWidth(0.6f)
-                .background(Color(0xFFB2FF59), shape = RoundedCornerShape(16.dp))
-                .height(40.dp)
-        ) {
-            Text(text = "SALIR", fontSize = 14.sp)
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.LightGray, shape = RoundedCornerShape(8.dp))
+                .background(Color(0xFFAED581), shape = RoundedCornerShape(8.dp))
                 .padding(16.dp)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFFB2FF59), shape = RoundedCornerShape(8.dp))
+                    .background(Color(0xFFAED581), shape = RoundedCornerShape(8.dp))
                     .padding(8.dp)
             ) {
                 Text(
-                    text = "NIMBRE DE LA ACTIVIDAD",
-                    color = Color.DarkGray,
+                    text = "NOMBRE DE LA ACTIVIDAD.",
+                    color = Color(0xFF33691E),
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
@@ -106,7 +94,7 @@ fun InscripcionScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(100.dp)
-                    .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+                    .background(Color(0xFFC5E1A5), shape = RoundedCornerShape(8.dp))
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -114,29 +102,115 @@ fun InscripcionScreen() {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFFB2FF59), shape = RoundedCornerShape(8.dp))
+                    .background(Color(0xFFAED581), shape = RoundedCornerShape(8.dp))
                     .padding(8.dp)
             ) {
-                Text(text = "NOMBRE:_____________")
-                Text(text = "APELLIDO:_____________")
-                Text(text = "GMAIL:_____________")
-                Text(text = "TELEFONO:_____________")
-                Text(text = "ID:_____________")
+                val nombre = remember { mutableStateOf(TextFieldValue()) }
+                val apellido = remember { mutableStateOf(TextFieldValue()) }
+                val email = remember { mutableStateOf(TextFieldValue()) }
+                val telefono = remember { mutableStateOf(TextFieldValue()) }
+                val id = remember { mutableStateOf(TextFieldValue()) }
+
+                TextField(
+                    value = nombre.value,
+                    onValueChange = { nombre.value = it },
+                    label = { Text("NOMBRE") },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color(0xFFD6E6A7)
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                TextField(
+                    value = apellido.value,
+                    onValueChange = { nombre.value = it },
+                    label = { Text("APELLIDO") },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color(0xFFD6E6A7)
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                TextField(
+                    value = email.value,
+                    onValueChange = { nombre.value = it },
+                    label = { Text("EMAIL") },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color(0xFFD6E6A7)
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                TextField(
+                    value = telefono.value,
+                    onValueChange = { nombre.value = it },
+                    label = { Text("TELEFONO") },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color(0xFFD6E6A7)
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                TextField(
+                    value = id.value,
+                    onValueChange = { nombre.value = it },
+                    label = { Text("ID") },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color(0xFFD6E6A7)
+                    ),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-
-        Button(
-            onClick = {  },
-            modifier = Modifier
-                .fillMaxWidth(0.6f)
-                .background(Color.LightGray, shape = RoundedCornerShape(16.dp))
-                .height(40.dp)
+        Spacer(modifier = Modifier.weight(1f))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "INSCRIBITE", fontSize = 14.sp, color = Color.DarkGray)
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA3D16A))
+            ) {
+                Text(text = "INSCRIBETE", color = Color.White, fontWeight = FontWeight.Bold)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(horizontal = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA3D16A))
+            ) {
+                Text(text = "SALIR", color = Color.White, fontWeight = FontWeight.Bold)
+            }
         }
     }
 }
+
 
